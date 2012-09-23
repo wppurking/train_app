3ba0187918cbc697e43c6fe4f3d1d60ff7d0b016
wyatt <wppurking@gmail.com>
2012-09-23 12:39:33 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TrainApp::Application.config.secret_token = 'fd077fa4681081820efb1a1213daecc4a748d33a23f5a5e54d0f14b1275499e6442980c4cfe70f8511580ca12392affb4aeead74d2484670b48632c6f4241f7e'
