6951e46e3a50b52aa9fbd87113d932b562debe28
wyatt <wppurking@gmail.com>
2012-10-14 23:41:14 +0800

    * TODO

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -11,6 +11,7 @@ describe UsersController do
 
       it "update another user should redirect to root path" do
         sign_in(user)
+        # TODO 卡在这里了, put 后请求返回的 response 查看, 明显是属于没有登陆被拦截的情况.
         put :update, id: user, user: FactoryGirl.attributes_for(:admin)
         response.should redirect_to root_url
       end
